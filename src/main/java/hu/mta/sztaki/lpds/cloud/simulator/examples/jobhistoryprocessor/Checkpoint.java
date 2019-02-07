9ad617f7d9a4c32ba82da90619b49052c9b08e5f
Aaron Dalzell <aarondalzell16@gmail.com>
2019-02-07 16:46:33 +0000

    Two new classes.
    To be used for checkpointing technique.

---
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/examples/jobhistoryprocessor/Checkpoint.java
@@ -0,0 +1,34 @@
+/*
+ *  ========================================================================
+ *  DISSECT-CF Examples
+ *  ========================================================================
+ *  
+ *  This file is part of DISSECT-CF Examples.
+ *  
+ *  DISSECT-CF Examples is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or (at
+ *  your option) any later version.
+ *  
+ *  DISSECT-CF Examples is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with DISSECT-CF Examples.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  (C) Copyright 2015, Gabor Kecskemeti (kecskemeti.gabor@sztaki.mta.hu)
+ */
+package hu.mta.sztaki.lpds.cloud.simulator.examples.jobhistoryprocessor;
+
+import hu.mta.sztaki.lpds.cloud.simulator.DeferredEvent;
+import hu.mta.sztaki.lpds.cloud.simulator.helpers.job.Job;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VMManager;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.resourcemodel.ResourceConsumption;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.resourcemodel.ResourceConsumption.ConsumptionEvent;
+
+public class Checkpoint {
+	
+}
